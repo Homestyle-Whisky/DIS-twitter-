bce066314071b086d27843abdfefcfc44cef5d26
Tommy3662 <125194938+Tommy3662@users.noreply.github.com>
2025-05-22 15:02:25 +0200

    third frontend

--- frontend.js
+++ frontend.js
@@ -1,1 +1,7 @@
+const createAccountBtn = document.querySelector(".create-account");
+const slide1 = document.querySelector(".slide-1");
 
+createAccountBtn.addEventListener("click", (e) => {
+  e.preventDefault(); // prevent form from submitting
+  slide1.classList.add("hidden");
+});
