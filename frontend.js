b96d3df268e1e0311e52b0c0f49e476202d69818
Felixnielsen <felix.h.nielsen@gmail.com>
2025-05-27 15:57:51 +0200

    Update frontend.js

--- frontend.js
+++ frontend.js
@@ -17,3 +17,37 @@ nextPage.forEach((button) => {
     allPages[currentPageIndex].classList.remove("hidden");
   });
 });
+
+
+
+
+
+
+// Function to send guess to Flask backend
+async function sendGuess(user_id, tweet_id, user_answer, correct_answer) {
+  const response = await fetch("http://localhost:5000/submit_guess", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      user_id: user_id,
+      tweet_id: tweet_id,
+      user_answer: user_answer,
+      correct_answer: correct_answer
+    })
+  });
+
+  const result = await response.json();
+  console.log("Flask-svar:", result);
+}
+
+// Testfunktion kaldet af knappen
+function testGuess() {
+  sendGuess(
+    "075db9a5-1b1f-40ed-aa07-e5f37ccedc5c", // user_id
+    42,                                     // tweet_id
+    true,                                   // user_answer
+    false                                   // correct_answer
+  );
+}
