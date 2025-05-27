53d8595f33832a6ee92f6fec6d16cf88647bcdb8
JonasHagelskjaerGodskesen <tbl650@alumni.ku.dk>
2025-05-27 12:25:38 +0200

    Python funktioner

--- src/Databaseconnection.py
+++ src/Databaseconnection.py
@@ -1,31 +1,65 @@
-import psycopg2
-from dotenv import load_dotenv
 import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
 
-# Load environment variables from .env
 load_dotenv()
 
-dsn = os.getenv("DATABASE_URL")            # ← now this will be your full URI
+url: str = os.getenv("SUPABASE_URL")
+key: str = os.getenv("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
 
-# Connect to the database
+response = (
+    supabase.table("tweet_data")
+    .select("*")
+    .execute()
+)
 
-connection = psycopg2.connect(dsn, sslmode="require")
-
-print("Connection successful!")
+def user_signup(email:str , password: str, name: str): 
     
-    # Create a cursor to execute SQL queries
-cursor = connection.cursor()
+    credentials= {
+        "email": email, 
+        "password": password,
+        "data":{ "name": name
+            }
+        }
+    response = supabase.auth.sign_up(credentials) 
+    print("user created", response.user)
+
+    auth_resp = supabase.auth.sign_up(credentials)
+
+    user_uuid = auth_resp.user.id
+
+    insert_to_users = (supabase.table("users")
+                       .insert({"username": name,
+                                "password": password, 
+                                "mail": email,
+                                "id": user_uuid
+                                })
+                        .execute() 
+                       )
+    return response, insert_to_users
+
+user_signup("jonasg300@gmail.com", "jonashahah", "batman123")
+
+
+# def user_login(email,password): 
+
+
+
+
+def get_tweet_attributes(tweet_id: str): 
+    response = (supabase.table("tweet_data")
+                .select("name", "tweet", "is_real", "image_url")
+                .eq("Tweet_ID", tweet_id)
+                .execute()
+        ) 
+    return response.data
+
+
+print(get_tweet_attributes("1"))
+
+
+
     
-
-cursor.execute("select tweet from tweet_data;")
-rows = cursor.fetchall()
-
-for i in rows: 
-    print(i)
-
-    # Close the cursor and connection
-cursor.close()
-connection.close()
-print("Connection closed.")
-
+# user_signup("jonasg300@gmail.com", "madsmadpakke", "batman")
 
